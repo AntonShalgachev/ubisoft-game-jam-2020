a1b987f9b06bec75611a5b66ddf0ba515a14742b
Anton Shalgachev <anton.shalgachev@gmail.com>
2020-11-28 21:33:56 +0100

    desync bar

---
+++ Assets/Scripts/UI/DesyncBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityPrototype
+{
+    public class DesyncBar : MonoBehaviour
+    {
+        [SerializeField] private Slider m_bar;
+
+        private void Update()
+        {
+            var desyncManager = GameComponentsLocator.Get<DesynchronizationManager>();
+            m_bar.value = desyncManager.desynchronization;
+        }
+    }
+}
