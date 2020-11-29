2d34d3fddc63548a47cd01c75b8d5dc372a94bf3
Anton Shalgachev <anton.shalgachev@gmail.com>
2020-11-29 17:15:54 +0100

    new level design

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -7,6 +7,7 @@ namespace UnityPrototype
     public class CelestialBody : MonoBehaviour
     {
         [SerializeField] private CelestialBodyContainer m_celestialBodyGroup = null;
+        [SerializeField] private float m_radius = 0.0f;
 
         public SimulatedObject simulatedObject => GetComponent<SimulatedObject>();
 
@@ -16,7 +17,8 @@ namespace UnityPrototype
         {
             var targetObjectPos = targetObject.position;
             var selfPos = simulatedObject.position;
-            var r2 = (targetObjectPos - selfPos).sqrMagnitude;
+            var r = (targetObjectPos - selfPos).magnitude - m_radius;
+            var r2 = r * r;
 
             var forceDirection = (selfPos - targetObjectPos).normalized;
             var forceMagnitude = m_simulationManager.gravitationalConstant * simulatedObject.mass * targetObject.mass / r2;
@@ -49,6 +51,12 @@ namespace UnityPrototype
             {
                 m_celestialBodyGroup.UnregisterBody(this);
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            GizmosHelper.DrawCircle(transform.position, m_radius);
         }
     }
 }
